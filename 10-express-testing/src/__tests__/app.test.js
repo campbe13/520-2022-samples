d26348eedddb134013a739936ff7e199a8d23c19
Patricia Campbell <pcampbell.edu@gmail.com>
2022-11-04 11:17:02 -0400

    added post route & test

--- 10-express-testing/src/__tests__/app.test.js
+++ 10-express-testing/src/__tests__/app.test.js
@@ -2,16 +2,42 @@ import request from "supertest"
 import app from "../app.js"
 
 describe ("GET /api", () => {
+  test("Should respond with 404", async () =>  {
+    let resp = await request(app).get("/api")
+    expect(resp.statusCode).toBe(404)
+  });
+});
+
+describe ("GET /api/?name=Fozzie", () => {
   test("Should respond with JSON", async () =>  {
-    // ?? data
-    const query  = {
-      name: "Fozzie"
-    }
-    const result = {
-      message: "Hello Fozzie"
-    }
-    resp = await request(app).get("/api").send(query)
-    expect(resp.statusCode).tobe(200)
+    let result  = { message: "Hello Fozzie"}
+    let resp = await request(app).get("/api/?name=Fozzie")
+    expect(resp.statusCode).toBe(200)
     expect(resp.body).toEqual(result)
+  });
+});
+// fix code ??
+describe ("GET /api/?name=", () => {
+  test("Should respond with 404", async () =>  {
+    let resp = await request(app).get("/api/?name=")
+    expect(resp.statusCode).toBe(404)
+  });
+});
+
+describe ("GET /api/?xyzname=Fozzie", () => {
+  test("Should respond with 404", async () =>  {
+    let resp = await request(app).get("/api/?xyzname=Fozzie")
+    expect(resp.statusCode).toBe(404)
+  });
+});
+
+describe("POST /api/addname ", () => {
+  test("It should respond with 201", async () => {
+    //data not really needed
+    const data = {
+      name: "Kermit"
+    };
+    const response = await request(app).post("/api/addname").send(data);
+    expect(response.statusCode).toBe(201);
   });
 });
--- 10-express-testing/src/routes/api.js
+++ 10-express-testing/src/routes/api.js
@@ -8,21 +8,38 @@
 */
 import express from "express"
 const router = express.Router();
+function addname (req, res) {
+  // stub
+  console.log(req.body)
+  res.status(201).json({ message: "added"})
+}
 /** 
 * check the request 
 * to see if there is a property `name`; 
 * if so, it sends a json response `{message: "Hello" + req.query.name}` 
 * If not, send a 404 with json error message `{error: "not supported"}`
+* change to 201 ??
  */
 
 function checkname (req, res) {
-  
+  /* 
+  fix */
+  if ('name' in req.query && req.query.name) {
+    res.json({ message: "Hello " + req.query.name })
+  } 
+  res.status(404).json({ error: "not supported" })
+}
+
+/* 
+  start with this
   if ('name' in req.query) {
     res.json({ message: "Hello " + req.query.name })
   } else {
     res.status(404).json({ error: "not supported" })
   }
-}
+  name= will return 200 / name=
+*/
+router.post("/addname", (req, res) => addname(req, res))
 router.get("/", (req, res) => checkname(req, res))
 
 // because I export as default, can name it anything on import
